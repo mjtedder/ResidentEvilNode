5fe01eadc4bc4876cb080c78a8627f9da3311999
mjtedder <mjtedder@gmail.com>
2018-06-26 09:50:26 -0400

    inquirer questions functioning properly

--- start.js
+++ start.js
@@ -12,7 +12,7 @@ console.log('***RESIDENT EVIL NODE***' + '\n')
 console.log('Leon S. Kennedy HP: ' + playerHP + '\n' + 'Mr. X HP: ' + zombieHP)
 console.log('======================================================')
 
-// function for calculating damage
+// function for calculating damage, randomg number between 1 and 5
 
 function rollDamage(){
 let attack = Math.floor(Math.random()* 5+1)
@@ -21,11 +21,18 @@ startGame(attack, zombieAttack)
 }
 
 //function for start game - use inquirer
-//function startGame(attack, zombieAttack) {
-    //let questions = [
+function startGame(attack, zombieAttack) {
+    let questions = [
+           {
+            type: 'list',
+            name: 'damage',
+            message: 'Choose a number:',
+            choices: ['1','2','3','4','5']
+           }
+        ]
+    inquirer.prompt(questions)
+    }
 
-    //]
-//}
 
 
 
