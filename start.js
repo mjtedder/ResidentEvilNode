92aa805033bbb652d5c75408c53da37432e88dfa
mjtedder <mjtedder@gmail.com>
2018-06-27 11:03:43 -0400

    todo: get restart game working

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ let fs = require('fs')
 let inquirer = require('inquirer')
 
 // set global variables & starting HP
+
 let minHP = 20
 let maxHP = 40
 let playerHP = Math.floor(Math.random()*(maxHP - minHP +1))+minHP
@@ -15,13 +16,13 @@ console.log('======================================================')
 // function for calculating damage, randomg number between 1 and 5
 
 function rollDamage() {
-let attack = Math.floor(Math.random()* 5+1)
-let zombieAttack = Math.floor(Math.random() * 5+1)
-startGame(attack, zombieAttack)
+    let attack = Math.floor(Math.random()* 5+1)
+    let zombieAttack = Math.floor(Math.random() * 5+1)
+    startBattle(attack, zombieAttack)
 }
 
 //function for start game - use inquirer
-function startGame(attack, zombieAttack) {
+function startBattle(attack, zombieAttack) {
     let questions = [
            {
             type: 'list',
@@ -52,11 +53,13 @@ function startGame(attack, zombieAttack) {
             console.log('Oh no! Leon is dead!')
             console.log('=====================================================')
         } else if ((zombieHP - attack) <=0) {
-            console.log('Mr. X is dead! You survived!')
+            console.log('Mr. X is dead! Leon survived!')
+            console.log('=====================================================')
         }
     })
 }
-startGame()
+
+rollDamage()
 
 
 
